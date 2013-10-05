81e897adae448b9a68a2826b67420ac0d01da8f1
mekzcnt <s6070022@kmitl.ac.th>
2013-10-06 00:41:39 +0700

    Checking card names in a deck.~~

--- poker.py
+++ poker.py
@@ -19,4 +19,21 @@ class Card(object):
         t1 = self.suit, self.rank
         t2 = other.suit, other,rank
         return cmp(t1, t2)
+
+class Deck(object):
+    """Represents a deck of card."""
+
+    def __init__(self):
+        self.cards = []
+        for suit in range(4):
+            for rank in range(1, 14):
+                card = Card(suit, rank)
+                self.cards.append(card)
+
+    def __str__(self):
+        res = []
+        for card in self.cards:
+            res.append(str(card))
+        return '\n'.join(res)
+    
     
