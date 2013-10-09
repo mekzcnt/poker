ef76e036fafac2f824d3d65f248cfd8db144f2d0
neotong <s6070029@kmitl.ac.th>
2013-10-09 20:07:53 +0700

    Fixed too long message for easy to edit and working

--- poker.py
+++ poker.py
@@ -1,9 +1,9 @@
 class Card(object):
     """Represents a standard playing card."""
 
-    suit_names = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
-    rank_names = [None, 'Ace', '2', '3', '4', '5', '6', '7',
-                  '8', '9', '10', 'Jack', 'Queen', 'King']
+    suit_names = ['C', 'D', 'H', 'S']
+    rank_names = [None, 'A', '2', '3', '4', '5', '6', '7',
+                  '8', '9', '10', 'J', 'Q', 'K']
 
     def __init__(self, suit=0, rank=2):
         self.suit = suit
@@ -11,7 +11,7 @@ class Card(object):
 
     def __str__(self):
         """Returns readable string"""
-        return '%s of %s' % (Card.rank_names[self.rank],
+        return '%s%s' % (Card.rank_names[self.rank],
                              Card.suit_names[self.suit])
 
     def __cmp__(self, other):
